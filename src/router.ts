bdeff22100bedd23c85b7c6b2995f408ef498132
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2022-05-12 11:52:06 -0300

    teste
    teste

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,15 @@
-import { Router, Request } from "express";
+import { Router, Request, Response } from "express";
 const app = Router()
 
-
-app.get('/test', (req, res) => {
+app.post('/send', (req: Request, res: Response) => {
+    const { body } = req
+    return res.status(200).json({ dados: body, msg: "Dados enviados!" })
+})
+app.get('/test', (req: Request, res: Response) => {
     return res.status(200).json({ msg: 'hello world' })
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     return res.status(200).json({ msg: 'pagina principal' })
 })
 export default app
