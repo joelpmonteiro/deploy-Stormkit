a40a9721dbc3cb42392aa6b0e58f487565d9f784
joel de paula monteiro <37256731+joelpmonteiro@users.noreply.github.com>
2022-05-12 12:01:13 -0300

    router
    router

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,13 @@ app.post('/send', (req: Request, res: Response) => {
     const { body } = req
     return res.status(200).json({ dados: body, msg: "Dados enviados!" })
 })
-app.get('/test', (req: Request, res: Response) => {
+
+app.get('/send/:id', (req: Request, res: Response) => {
+    const { id } = req.params
+    return res.status(200).json({ dados: id, msg: "Dados enviados!" })
+})
+
+app.get('/get', (req: Request, res: Response) => {
     return res.status(200).json({ msg: 'hello world' })
 })
 
